a588488166cce3b01d3ecc5a5384efcbe6b89470
Ghazala Jamal <129306652+ghazala16@users.noreply.github.com>
2025-07-15 15:36:37 +0530

    Update app.js

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -3,49 +3,47 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const pinoLogger = require('./logger');
-
 const connectToDatabase = require('./models/db');
-// Removed loadData since it's unused
-// const { loadData } = require('./util/import-mongo/index');
 
 const app = express();
-app.use('*', cors());
 const port = 3063;
 
-// Connect to MongoDB
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Logger
+const pinoHttp = require('pino-http');
+app.use(pinoHttp({ logger: pinoLogger }));
+
+// Database connection
 connectToDatabase()
     .then(() => {
         pinoLogger.info('Connected to DB');
     })
-    .catch((e) => console.error('Failed to connect to DB', e));
+    .catch((e) => pinoLogger.error('Failed to connect to DB', e));
 
-app.use(express.json());
-
-// Route files
+// Routes
 const secondChanceItemsRoutes = require('./routes/secondChanceItemsRoutes');
 const authRoutes = require('./routes/authRoutes');
 const searchRoutes = require('./routes/searchRoutes');
 
-const pinoHttp = require('pino-http');
-const logger = require('./logger');
-
-app.use(pinoHttp({ logger }));
-
-// Use Routes
 app.use('/api/secondchance/items', secondChanceItemsRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/secondchance/search', searchRoutes);
 
-// Global Error Handler
-app.use((err, req, res, next) => {
-    console.error(err);
-    res.status(500).send('Internal Server Error');
-});
-
+// Basic endpoint
 app.get('/', (req, res) => {
     res.send('Inside the server');
 });
 
+// Global Error Handler
+app.use((err, req, res, next) => {
+    pinoLogger.error(err);
+    res.status(500).send('Internal Server Error');
+});
+
+// Start server
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+    pinoLogger.info(`Server running on port ${port}`);
 });
